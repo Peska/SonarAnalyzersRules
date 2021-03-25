0a908f36d8612c4b90503cb9d1b3beabb24ba2a3
Peska <peska_jr@o2.pl>
2021-03-25 22:17:12 +0100

    Add project files.

---
+++ SonarAnalyzersRules/Program.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SonarAnalyzersRules
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] files = Directory.GetFiles(@"C:\Users\Peska\source\repos\sonar-dotnet\analyzers\rspec\cs", "*.json");
+
+			List<Rule> allRules = new(files.Length);
+
+			foreach (string file in files)
+				allRules.Add(JsonConvert.DeserializeObject<Rule>(File.ReadAllText(file)));
+
+			StringBuilder builder = new StringBuilder();
+
+			foreach (IGrouping<string, Rule> group in allRules.GroupBy(x => x.type))
+			{
+				builder.AppendLine($"# {group.Key}\n\n");
+
+				string severity = GetSeverity(group.Key);
+
+				foreach (var rule in group.OrderBy(x => x.defaultSeverity))
+				{
+					if (rule.sqKey == null || rule.sqKey[0] != 'S')
+						continue;
+
+					builder.AppendLine($"# {rule.sqKey}: {rule.defaultSeverity} :: {rule.title}");
+					builder.AppendLine($"dotnet_diagnostic.{rule.sqKey}.severity = {severity}\n");
+				}
+
+				builder.AppendLine();
+			}
+
+			File.WriteAllText(@"C:\Users\Peska\source\repos\SonarAnalyzersRules\SonarAnalyzersRules\rules.txt", builder.ToString());
+		}
+
+		public static string GetSeverity(string severity)
+		{
+			switch (severity)
+			{
+				case "CODE_SMELL": return "warning";
+				case "BUG": return "error";
+				case "SECURITY_HOTSPOT": return "error";
+				case "VULNERABILITY": return "error";
+				default: return string.Empty;
+			}
+		}
+	}
+}
---
+++ SonarAnalyzersRules/Rule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarAnalyzersRules
+{
+	public class Rule
+	{
+		public string title { get; set; }
+		public string type { get; set; }
+		public string status { get; set; }
+		public Remediation remediation { get; set; }
+		public string[] tags { get; set; }
+		public string defaultSeverity { get; set; }
+		public string ruleSpecification { get; set; }
+		public string sqKey { get; set; }
+		public string scope { get; set; }
+	}
+
+	public class Remediation
+	{
+		public string func { get; set; }
+		public string constantCost { get; set; }
+	}
+}
